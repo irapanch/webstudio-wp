bea2fb030f21bfdac7d1ada8e7e379c25b9b3678
irapanch <117355580+irapanch@users.noreply.github.com>
2024-08-15 16:06:57 +0300

    fix path

--- wp-content/themes/web_test/functions.php
+++ wp-content/themes/web_test/functions.php
@@ -7,8 +7,8 @@ function () {
     wp_enqueue_style( 'fonts-gstatic', 'https://fonts.gstatic.com' );
     wp_enqueue_style( 'fonts', 'https://fonts.googleapis.com/css2?family=Raleway:wght@800&family=Roboto:wght@400;500;700;900&display=swap' );
     wp_enqueue_style( 'normalize', 'https://cdn.jsdelivr.net/npm/modern-normalize@2.0.0/modern-normalize.min.css' );
-	// wp_enqueue_style( 'styles', get_template_directory_uri() . '/assets/css/styles.css' );
-    wp_enqueue_style( 'styles', 'https://irapanch.github.io/webstudio-wp/wp-content/themes/web_test/assets/css/styles.css' );
+	wp_enqueue_style( 'styles', get_template_directory_uri() . '/assets/css/styles.css' );
+    
 
 	wp_enqueue_script( 'mobile-menu', get_template_directory_uri() . '/assets/js/mobile-menu.js', array(), '1.0.0', true );
     wp_enqueue_script( 'modal.js', get_template_directory_uri() . '/assets/js/modal.js', array(), '1.0.0', true );
